5dd006204670678eec66dc26df0c8e8842a2e6da
aleksgpak <aleksgpak@gmail.com>
2023-01-24 23:01:31 +0600

    Initial Commit

---
+++ ToDoManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoManager
+//
+//  Created by Aleksandr Pak on 24/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
